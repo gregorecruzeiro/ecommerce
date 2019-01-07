92cfc34eb9dd08c4cf24b0a99a21c561a51d1ada
Gregore Cruzeiro <degloriocg@gmail.com>
2019-01-07 00:43:48 -0200

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -86,6 +86,28 @@ class Category extends Model {
 		}
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 3)
+	{
+		$start = ($page - 1) * $itemsPerPage;
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+		", [
+			':idcategory'=>$this->getidcategory()
+		]);
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+	}			
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
@@ -94,6 +116,8 @@ class Category extends Model {
 			':idproduct'=>$product->getidproduct()
 		]);
 	}
+
+
 	public function removeProduct(Product $product)
 	{
 		$sql = new Sql();
@@ -101,7 +125,7 @@ class Category extends Model {
 			':idcategory'=>$this->getidcategory(),
 			':idproduct'=>$product->getidproduct()
 		]);
-	}					
+	}			
 
 }
 ?>
