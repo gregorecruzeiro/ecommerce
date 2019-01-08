4c18e6cc3b58ab2c11893b1904dab23bc28f63f0
Gregore Cruzeiro <degloriocg@gmail.com>
2019-01-07 23:19:37 -0200

    Site Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -289,13 +289,24 @@ public static function getForgot($email, $inadmin = true)
 		$_SESSION[User::ERROR_REGISTER] = NULL;
 	}
 
+
+	public static function checkLoginExist($login)
+	{
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+			':deslogin'=>$login
+		]);
+		return (count($results) > 0);
+	}
+	
+
 	public static function getPasswordHash($password)
 	{
 		return password_hash($password, PASSWORD_DEFAULT, [
 			'cost'=>12
 		]);
 	}
-			
+
 
 }
 ?>
