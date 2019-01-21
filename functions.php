d07d5c0d3b4b0db14607426ed4ea4c80dcfb954f
Gregore Cruzeiro <degloriocg@gmail.com>
2019-01-21 16:47:46 -0200

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -6,6 +6,10 @@ function formatPrice($vlprice)
 	if (!$vlprice > 0) $vlprice = 0;
 	return number_format($vlprice, 2, ",", ".");
 }
+function formatDate($date)
+{
+	return date('d/m/Y', strtotime($date));
+}
 function checkLogin($inadmin = true)
 {
 	return User::checkLogin($inadmin);
